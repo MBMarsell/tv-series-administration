a5f05ff3aa7a91ab89f46948cc8d1d60e3ecd850
Martin Marsell <martbustmar@gmail.com>
2023-04-20 00:53:18 +0200

    Add/start on the controller, lotta work to do here

---
+++ webapi/Controllers/TVShowController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Services;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TVShowsController : ControllerBase
+    {
+        private readonly TVShowServices _tvShowServices;
+
+        public TVShowsController(TVShowServices tvShowServices)
+        {
+            _tvShowServices = tvShowServices;
+        }
+
+        // api/TVShows
+        [HttpGet]
+        public ActionResult GetAllTVShows()
+        {
+            return Ok(_tvShowServices.GetAllTVShows());
+        }
+
+
+    }
+}
