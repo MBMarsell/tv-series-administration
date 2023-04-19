1a7010daf5c7ff75b13fa6a7d41f759a3bd7ee71
Martin Marsell <martbustmar@gmail.com>
2023-04-20 00:49:34 +0200

    Add models for TVShow and uploading file

---
+++ webapi/Models/TVShowModels.cs
@@ -0,0 +1,30 @@
+﻿namespace webapi.Models
+{
+    public class TVShowModel
+    {
+        public string ShowName { get; set; }
+        public string Summary { get; set; }
+        public double Rating { get; set; }
+        public string Network { get; set; }
+        public ICollection<string> Genres { get; set; }
+        public int episodeCount { get; set; }
+        public int episodesReleasedCount { get; set; }
+    }
+
+    public class TVShowNetwork
+    {
+        public double AvgRating { get; set; }
+        public string NetworkName { get; set; }
+        public string ShowName { get; set; }
+        public string ShowCount { get; set; }
+    }
+
+    public class TVShowEpisode
+    {
+        public string ShowName { get; set; }
+        public int Season { get; set; }
+        public int Number { get; set; }
+        public DateTime Airdate { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
