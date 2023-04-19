9dbb1a31b4589f1df0220aa054606dc0bbe68a3e
Martin Marsell <martbustmar@gmail.com>
2023-04-20 00:54:48 +0200

    Add needed services to container

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,8 +1,15 @@
+using webapi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<TVShowServices>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
