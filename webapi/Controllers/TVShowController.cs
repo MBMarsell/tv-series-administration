cbedcbe493f31196afc6be985b270c55cc2f81de
Martin Marsell <martbustmar@gmail.com>
2023-04-21 00:36:08 +0200

    Add api/TVShows/Show in TVShowController.cs, currently works with the service

--- webapi/Controllers/TVShowController.cs
+++ webapi/Controllers/TVShowController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
 using webapi.Services;
 
 namespace webapi.Controllers
@@ -16,9 +17,36 @@ namespace webapi.Controllers
 
         // api/TVShows
         [HttpGet]
-        public ActionResult GetAllTVShows()
+        public ActionResult<List<TVShowModel>> GetAllTVShows()
         {
             return Ok(_tvShowServices.GetAllTVShows());
+            
+        }
+
+        //api/TVShows/Episode
+       [HttpGet("Episodes")]
+        public ActionResult<List<TVShowEpisode>> GetEpisodes()
+        {
+            return _tvShowServices.GetEpisodes();
+        }
+
+        //api/TVShows/Show
+        [HttpGet("Show")]
+        public ActionResult<TVShow> GetTVShow(string showTitle)
+        {
+            if(string.IsNullOrEmpty(showTitle))
+            {
+                return BadRequest("Show title cannot be null or empty");
+            }
+
+            var tvShow = _tvShowServices.GetTVShow(showTitle);
+
+            if(tvShow == null)
+            {
+                return NotFound("Tv show not found");
+            }
+
+            return Ok(tvShow);
         }
 
 
