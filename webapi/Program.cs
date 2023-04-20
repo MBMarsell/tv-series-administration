36db51006c1d09b37f111cc2c4eda900323181fa
Martin Marsell <martbustmar@gmail.com>
2023-04-21 00:34:37 +0200

    Add CORS and RateLimiting in program.cs

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using webapi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,28 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddScoped<TVShowServices>();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Add rate limiting services
+builder.Services.AddMemoryCache();
+
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+builder.Services.AddInMemoryRateLimiting();
+
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +44,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 }
+
+app.UseIpRateLimiting();
 
 app.UseHttpsRedirection();
 
